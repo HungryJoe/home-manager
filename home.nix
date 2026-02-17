b6e5167a15f607054be3a804785ba6ca792785f1
Sam Kagan <skagan@nrao.edu>
2026-02-17 11:28:36 -0800

    Removed @angular/cli since Nix dropped support for it

--- home.nix
+++ home.nix
@@ -78,7 +78,6 @@ home = {
     # Angular
     nodejs_22  # Supports Angular v18 (archive) and v20 (WS post-upgrade) (Src: https://angular.dev/reference/versions)
     nodePackages.typescript-language-server
-    nodePackages."@angular/cli"
     # Nix doesn't currently have @angular/language-server in its package repo,
     #   and anyways installing specific versions of it is easier through npm.
 
